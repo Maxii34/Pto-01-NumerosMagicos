4143711de2c7305fce3008feaa89ad564211d207
Maxi Ordoñez <exemaxi@outlook.com>
2025-07-09 14:53:57 -0300

    Se agrego maquetado, css y codigo del btn thema

---
+++ js/01-tp-nummagicos.js
@@ -0,0 +1,11 @@
+// Modo oscuro / claro
+const checkbox = document.querySelector('.theme-checkbox');
+
+checkbox.addEventListener('change', () => {
+  const theme = checkbox.checked ? 'dark' : 'light';
+  document.documentElement.setAttribute('data-bs-theme', theme);
+});
+
+// Función del juego 
+
+
